2d480686a37d436d305cfa6d29a1dbbe56f6b9a0
Henry Paulino <henrypl360@gmail.com>
2020-05-26 16:02:17 -0400

    Initial Commit

---
+++ TeslaWatchRemote WatchKit Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  TeslaWatchRemote WatchKit Extension
+//
+//  Created by Henry Paulino on 5/26/20.
+//  Copyright © 2020 Henry Paulino. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
---
+++ TeslaWatchRemote WatchKit Extension/NotificationView.swift
@@ -0,0 +1,21 @@
+//
+//  NotificationView.swift
+//  TeslaWatchRemote WatchKit Extension
+//
+//  Created by Henry Paulino on 5/26/20.
+//  Copyright © 2020 Henry Paulino. All rights reserved.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct NotificationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationView()
+    }
+}
